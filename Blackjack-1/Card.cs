44dbd8f5a7af8cc17dbc317d9168e4b9dfd1034a
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-03 10:21:09 +0900

    카드 공백 수정

--- Blackjack-1/Card.cs
+++ Blackjack-1/Card.cs
@@ -10,34 +10,24 @@ class Card
 
     public char Shape => _shapes;
     public int Value => _value;
-    public Card(char shape, int value)
-    {
+    public Card(char shape, int value) {
         _shapes = shape;
         _value = value;
     }
-    public override string ToString()
-    {
+    public override string ToString() {
         string cardNumber;
-        if(Value==11)
-        {
+        if (Value == 11) {
             cardNumber = "J";
-        }
-        else if(Value==12)
-        {
+        } else if(Value == 12) {
             cardNumber = "Q";
-        }
-        else if (Value==13)
-        {
+        } else if (Value == 13) {
             cardNumber = "K";
-        }
-        else if(Value ==1)
-        {
+        } else if(Value == 1) {
             cardNumber = "A";
-        }
-        else
-        {
+        } else {
             cardNumber = $"{Value}";
         }
-            return $"[{_shapes}{cardNumber}]";
+
+        return $"[{_shapes}{cardNumber}]";
     }
 }
