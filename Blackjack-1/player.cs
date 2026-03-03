5d355a68ac3a1adf01b662556e2a8329eaff9769
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-03 10:21:44 +0900

    함수 주석 추가, 공백 수정,

--- Blackjack-1/player.cs
+++ Blackjack-1/player.cs
@@ -10,40 +10,43 @@ class Player
     private int _count;
     public Player()
     {
-        _cardList=new Card[22];
+        _cardList = new Card[22];
         _count = 0;
     }
+    
+    /// <summary>
+    /// 카드 값을 Player의 내부 cardList에 저장
+    /// </summary>
+    /// <param name="card">  </param>
     public void AddCard(Card card)
     {
         _cardList[_count] = card;
         _count++;
     }
     public int Score { get; private set; }
+
+    /// <summary>
+    ///  점수 계산 후 Score변수에 저장
+    /// </summary>
     public void CheckScore()
     {
-        int acount = 0;
+        int aCount = 0;
         int score = 0;
-        for (int i = 0; i < _count; i++)
-        {
-            if (_cardList[i].Value > 10)
-            {
+        for (int i = 0; i < _count; i++) {
+            if (_cardList[i].Value > 10) {
                 score += 10;
-            }
-            else if (_cardList[i].Value == 1)
-            {
-                acount++;
+            } else if (_cardList[i].Value == 1) {
+                aCount++;
                 score += 11;
-            }
-            else if (_cardList[i].Value <= 10)
-            {
+            } else if (_cardList[i].Value <= 10) {
                 score += _cardList[i].Value;
             }
         }
         while (score > 21)
         {
-            if(acount>0)
+            if(aCount>0)
             {
-                acount--;
+                aCount--;
                 score -= 10;
             }
             else
@@ -53,29 +56,23 @@ class Player
         }
         Score = score;
     }
-    public string ShowCardList()
+
+    /// <summary>
+    /// 플레이어가 현재 가지고 있는 카드 리스트를 반환.
+    /// </summary>
+    /// <param name="isHidden">첫 카드를 히든으로 반환할지 여부</param>
+    /// <returns>카드 리스트</returns>
+    public string GetCardList(bool isHidden = false)
     {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < _count; i++)
-        {
+        for (int i = 0; i < _count; i++) {
+            if (isHidden && i == 0) {
+                sb.Append($"[??] ");
+            }
             sb.Append($"{_cardList[i]} ");
         }
 
         return sb.ToString();
     }
-    public string ShowHiddenCardList()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.Append("[??] ");
-
-        for (int i = 1; i < _count; i++)
-        {
-            sb.Append($"{_cardList[i]} ");
-        }
-
-        return sb.ToString();
-    }
-
-
 }
